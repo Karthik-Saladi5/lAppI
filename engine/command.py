c223a7c54e2403aeb87f89665ca6b7991f1e4ea2
SK's Laptop <karthiksaladi6@gmail.com>
2025-04-12 19:56:34 +0530

    Speech Recogntion

--- engine/command.py
+++ engine/command.py
@@ -1,5 +1,5 @@
 import pyttsx3
-
+import speech_recognition as sr
 def speak(text):
     engine=pyttsx3.init('sapi5')
     voices=engine.getProperty('voices')
@@ -10,4 +10,21 @@ def speak(text):
 
     engine.runAndWait()
 
-speak("Hello Boss")
+def takecommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print('listening....')
+        r.pause_threshold=1
+        r.adjust_for_ambient_noise(source)
+        audio=r.listen(source,10,6)
+
+    try:
+        print('recognizing')
+        query=r.recognize_google(audio,language='en-in')
+        print(f"user said: {query}")
+    except Exception as e:
+        return ""
+    return query.lower()
+
+text= takecommand()
+speak(text)
